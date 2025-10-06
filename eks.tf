4335f53a9e16157389baf0746b6d289017fe22c8
Muhammad Haroon <mh6046478@gmail.com>
2025-10-06 18:53:25 +0500

    Initial commit with Terraform EKS setup

---
+++ eks.tf
@@ -0,0 +1,36 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.24.0"
+
+  cluster_name    = local.name
+  cluster_version = "1.28"
+  cluster_endpoint_public_access = true
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  eks_managed_node_groups = {
+    default = {
+      desired_size = 2
+      max_size     = 3
+      min_size     = 1
+      instance_types = ["t2.medium"]
+    }
+  }
+
+  
+  access_entries = {
+    admin = {
+     principal_arn = data.aws_caller_identity.current.arn
+
+      policy_associations = {
+        admin = {
+          policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+          access_scope = {
+            type = "cluster"
+          }
+        }
+      }
+    }
+  }
+}
